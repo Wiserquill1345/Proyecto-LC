2c82a0b7f70ae40015ae11eb188b92cb9314c21f
UsagiAkukin <luisgithud@gmai.com>
2024-10-15 15:54:57 -0700

    Formulario para el de olvidar contraseña

---
+++ contraseña_olvidada.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/Styles.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/normalize.css">
+</head>
+<body>
+    <div class="formulario_Olvidar">
+        <h1>Olvido la contraseña?</h1>
+        <form action="POST">
+            <div class="correo">
+                <input type="email" id="miCorreo" require>
+                <label>Correo electronico</label>
+            </div>
+            <input type="submit" value="Enviar">
+        </form>
+    </div>
+</body>
+</html>
